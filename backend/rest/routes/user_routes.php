506331d8086ed2bc48bba8457d9573afca809703
farisgigic <faris.gigic@yahoo.com>
2025-04-14 14:05:54 +0200

    changes

--- backend/rest/routes/user_routes.php
+++ backend/rest/routes/user_routes.php
@@ -1,1 +1,10 @@
 <?php
+require_once __DIR__ . '/../services/UserService.class.php';
+
+Flight::set('userService', new UserService());
+Flight::group("/users", function() {
+    Flight::route('GET /all', function() {
+        $users = Flight::get('userService')->getAllUsers();
+        Flight::json($users, 200);
+    });
+})
--- backend/rest/services/UserService.class.php
+++ backend/rest/services/UserService.class.php
@@ -1,2 +1,38 @@
 <?php
 
+require_once __DIR__ . '/../dao/UserDao.class.php';
+
+class UserService {
+    
+    private $userDao;
+
+    public function __construct()
+    {
+        $this->userDao = new UserDao();
+    }
+
+    public function getUserById($id)
+    {
+        return $this->userDao->getUserById($id);
+    }
+
+    public function getAllUsers()
+    {
+        return $this->userDao->getAllUsers();
+    }
+
+    public function createUser($user)
+    {
+        return $this->userDao->createUser($user);
+    }
+
+    public function updateUser($id, $user)
+    {
+        return $this->userDao->updateUser($id, $user);
+    }
+
+    public function deleteUser($id)
+    {
+        return $this->userDao->deleteUser($id);
+    }
+}
