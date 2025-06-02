d5e38e3e5b5b090ba3c4ad416bc0aa4e7f6004da
Faris Gigic <faris.gigic@yahoo.com>
2025-06-02 18:14:21 +0200

    Deployment

--- backend/index.php
+++ backend/index.php
@@ -35,7 +35,7 @@ Flight::register("auth_middleware", "AuthMiddleware");
 Flight::route('OPTIONS *', function () {
     header('Access-Control-Allow-Origin: https://carcar-hw8hi.ondigitalocean.app');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    header('Access-Control-Allow-Headers: Content-Type, Authentication, X-Requested-With');
     header('Access-Control-Allow-Credentials: true');
     Flight::halt(200);
 });
