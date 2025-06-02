72f2a1d9ca8c1b06608340ac900a902ec2dae472
Faris Gigic <faris.gigic@yahoo.com>
2025-06-02 15:40:07 +0200

    Chaning Config class

--- backend/rest/routes/auth_routes.php
+++ backend/rest/routes/auth_routes.php
@@ -44,7 +44,7 @@ Flight::group("/auth", function () {
 
         $token = JWT::encode(
             $jwt_payload,
-            JWT_SECRET,
+            Config::JWT_SECRET(),
             'HS256'
         );
         Flight::response()->header('Authentication', $token);
@@ -73,7 +73,7 @@ Flight::group("/auth", function () {
             if (!$token)
                 Flight::halt(401, "Missing authentication header");
 
-            $decoded_token = JWT::decode($token, new Key(JWT_SECRET, 'HS256'));
+            $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), 'HS256'));
 
             Flight::json([
                 'jwt_decoded' => $decoded_token,
