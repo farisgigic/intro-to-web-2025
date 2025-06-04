f02977b46f17a47ed4de95becfd65f18c23cc16e
Faris Gigic <faris.gigic@yahoo.com>
2025-06-04 23:09:04 +0200

    Frontend and backend changes related to the forum posts

--- backend/index.php
+++ backend/index.php
@@ -33,6 +33,9 @@ Flight::register("carMaintenanceService", "CarMaintenanceService");
 Flight::register("forumService", "ForumService");
 Flight::register("auth_middleware", "AuthMiddleware");
 
+Flight::route("/fare", function () {
+    echo "Farecare";
+});
 header('Access-Control-Allow-Origin: https://intro-to-web-frontend-b3wd3.ondigitalocean.app');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authentication, X-Requested-With');
@@ -68,7 +71,6 @@ require_once __DIR__ . "/rest/routes/forum_routes.php";
 require_once __DIR__ . "/rest/routes/user_routes.php";
 require_once __DIR__ . "/rest/routes/car_routes.php";
 require_once __DIR__ . "/rest/routes/contact_routes.php";
-
 
 
 Flight::start();
